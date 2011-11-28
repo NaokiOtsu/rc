919eb1180df19a8db500e572402d6b2bc1b2ce7a
zentooo <ankerasoy@gmail.com>
2011-11-28 21:56:16 +0900

    add alabouter

---
+++ .vimperator/plugin/alabouter.js
@@ -0,0 +1,39 @@
+let PLUGIN_INFO =
+<VimperatorPlugin>
+<name>host-toggle-opener</name>
+<description>Alabouter</description>
+<version>1.0.0</version>
+<author>zentooo</author>
+<license>Creative Commons</license>
+<detail><![CDATA[
+    == Subject ==
+    This is the Alabouter.
+
+    == Commands ==
+    :alabouter
+]]></detail>
+</VimperatorPlugin>;
+
+
+liberator.modules.commands.addUserCommand(["alabouter"], "Alabouter",
+    function(args) {
+        var doc = content.document, loc = doc.location, timer;
+
+        function replace(element) {
+            [].forEach.call(doc.querySelectorAll('a[target="_blank"]'), function(el) {
+                el.setAttribute("href", el.textContent);
+            });
+        }
+
+        doc.body.addEventListener("AutoPagerAfterInsert", function(evt) {
+            if ( ! timer ) {
+                timer = setTimeout(function() {
+                    replace();
+                    timer = void 0;
+                }, 1000);
+            }
+        }, false);
+
+        replace();
+    }
+);
