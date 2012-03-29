d311dd6f99713d73c2bdea7716ab8a47f7726906
zentooo <ankerasoy@gmail.com>
2012-03-30 01:39:21 +0900

    add ore-ore plugin

---
+++ .vimperator/plugin/link-find-opener.js
@@ -0,0 +1,11 @@
+liberator.modules.commands.addUserCommand(["find"], "find link and open it", 
+    function(args) {
+        var str = args[0];
+        Array.prototype.forEach.call(
+            content.document.querySelectorAll('a[href*=' + str + ']'),
+            function(el) {
+                liberator.open(el.getAttribute("href"), liberator.NEW_TAB);
+            }
+        );
+    }
+);
